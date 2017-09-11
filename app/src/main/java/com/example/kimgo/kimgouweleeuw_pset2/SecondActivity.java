e57d2136e2ddb0b574d409c9bb3cb5bbf2f28cae
GoldenSimba97 <kim.gouweleeuw@gmail.com>
2017-09-11 16:24:08 +0200

    Poging 2 tot txt files uitlezen. Nog steeds niet gelukt

--- app/src/main/java/com/example/kimgo/kimgouweleeuw_pset2/SecondActivity.java
+++ app/src/main/java/com/example/kimgo/kimgouweleeuw_pset2/SecondActivity.java
@@ -5,19 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -26,30 +24,57 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        String content = null;
+        Scanner scanner = null;
+        String text = null;
+        try {
+            scanner = new Scanner( new File("madlib0_simple.txt") );
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (scanner != null) {
+            text = scanner.useDelimiter("\\A").next();
+        }
+        scanner.close();
+        Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT).show();
+
+
+//        InputStream targetStream = null;
+//        throws IOException {
+//            File initialFile = new File("src/main/resources/values/madlib0_simple.txt");
+//            try {
+//                targetStream = new FileInputStream(initialFile);
+//            } catch (FileNotFoundException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        Story story = new Story(targetStream);
+//        String text = toString();
+//        System.out.println(text);
+
+//        String content = null;
 //        try {
 //            content = new Scanner(new File("madlib0_simple.txt")).useDelimiter("\\Z").next();
 //        } catch (FileNotFoundException e) {
 //            e.printStackTrace();
 //        }
 
-        try(BufferedReader br = new BufferedReader(new FileReader("madlib0_simple.txt"))) {
-            StringBuilder sb = new StringBuilder();
-            String line = br.readLine();
+//        try(BufferedReader br = new BufferedReader(new FileReader("madlib0_simple.txt"))) {
+//            StringBuilder sb = new StringBuilder();
+//            String line = br.readLine();
+//
+//            while (line != null) {
+//                sb.append(line);
+//                sb.append(System.lineSeparator());
+//                line = br.readLine();
+//            }
+//            content = sb.toString();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
-            while (line != null) {
-                sb.append(line);
-                sb.append(System.lineSeparator());
-                line = br.readLine();
-            }
-            content = sb.toString();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println(content);
+//        System.out.println(content);
 //        System.out.println(content);
 //
 //        InputStream stream = null;
@@ -62,6 +87,25 @@ public class SecondActivity extends AppCompatActivity {
 //        System.out.println(text);
     }
 
+
+//    public void toInputStream() throws IOException {
+//        InputStream targetStream = null;
+//        File initialFile = new File("src/main/resources/values/madlib0_simple.txt");
+//        try {
+//            targetStream = new FileInputStream(initialFile);
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        Story story = new Story(targetStream);
+//        String text = toString();
+//        System.out.println(text);
+//    }
+
+
+
+
+
+
     public void goToNext(View view) {
         EditText edittext = (EditText) findViewById(R.id.editText);
         String text = edittext.getText().toString();
@@ -72,5 +116,15 @@ public class SecondActivity extends AppCompatActivity {
         intent.putExtra("ourString", text);
         startActivity(intent);
         finish();
+    }
+
+    public void printText(View view) {
+//        String content = null;
+//        try {
+//            content = new Scanner(new File("madlib0_simple.txt")).useDelimiter("\\Z").next();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+        Toast.makeText(getApplicationContext(),"hallo",Toast.LENGTH_SHORT).show();
     }
 }
