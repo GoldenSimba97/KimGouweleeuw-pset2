144a349610950bed7528c0e128561a8297076d54
GoldenSimba97 <kim.gouweleeuw@gmail.com>
2017-09-11 09:51:50 +0200

    Intent toegevoegd om terug te kunnen naar start

--- app/src/main/java/com/example/kimgo/kimgouweleeuw_pset2/SecondActivity.java
+++ app/src/main/java/com/example/kimgo/kimgouweleeuw_pset2/SecondActivity.java
@@ -5,16 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
     }
 
-    public void goToNext(View view) {
-        Intent intent = new Intent(this, SecondActivity.class);
+    public void goToFirst(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+        finish();
     }
 }
