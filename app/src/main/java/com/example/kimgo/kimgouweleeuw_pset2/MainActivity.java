f613ff7d5bfefebd2b14b95d87dc70d7a8bc4d92
GoldenSimba97 <kim.gouweleeuw@gmail.com>
2017-09-12 21:07:30 +0200

    Popup menu toegevoegd aan start

--- app/src/main/java/com/example/kimgo/kimgouweleeuw_pset2/MainActivity.java
+++ app/src/main/java/com/example/kimgo/kimgouweleeuw_pset2/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.kimgo.kimgouweleeuw_pset2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.view.MenuInflater;
 import android.view.View;
+//import android.widget.PopupMenu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +19,12 @@ public class MainActivity extends AppCompatActivity {
     public void goToNext(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
         startActivity(intent);
+    }
+
+    public void showPopUp(View view) {
+        PopupMenu popup = new PopupMenu(this, view);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.actions, popup.getMenu());
+        popup.show();
     }
 }
