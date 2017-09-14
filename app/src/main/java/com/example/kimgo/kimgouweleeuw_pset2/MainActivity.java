df0d8637d0507db0c8f1b80803824abf7c281aca
GoldenSimba97 <kim.gouweleeuw@gmail.com>
2017-09-14 10:49:21 +0200

    Layout verbeterd

--- app/src/main/java/com/example/kimgo/kimgouweleeuw_pset2/MainActivity.java
+++ app/src/main/java/com/example/kimgo/kimgouweleeuw_pset2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.kimgo.kimgouweleeuw_pset2;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.PopupMenu;
@@ -14,6 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switch (getResources().getConfiguration().orientation) {
+            case Configuration.ORIENTATION_PORTRAIT:
+                setContentView(R.layout.activity_main);
+                break;
+            case Configuration.ORIENTATION_LANDSCAPE:
+                setContentView(R.layout.landscape_main);
+                break;
+        }
     }
 
     public void goToNext(View view) {
